843286c283184a61efc9cff5c471d1acf374ff05
mahmoud ali <naruto.star3019@yahoo.com>
2023-09-06 00:36:14 +0300

    create project files

---
+++ script.js
@@ -0,0 +1,59 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent =""
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+
+    })
+})
+
+let currentwordindex = 0;
+let maxword = words.length -1;
+words[currentwordindex].style.opacity = ["1"];
+
+let changetext = ()=>{
+    let currentword = words[currentwordindex]
+    let nextword = currentwordindex === maxword ? words[0] : words[currentwordindex + 1]
+
+    Array.from(currentword.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        },i * 80);
+    })
+
+    nextword.style.opacity = "1";
+    Array.from(nextword.children).forEach((letter,i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340 + i * 80);
+    })    
+    
+    currentwordindex = currentwordindex === maxword ? 0 : currentwordindex + 1;
+}
+
+changetext()
+setInterval(changetext,3000)
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(let i = 0 ; i < dots ; i++){
+        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
+    }
+    elem.innerHTML = points;
+
+    const pointsmarked = elem.querySelectorAll('.points');
+    for(let i = 0; i<percent ; i++){
+        pointsmarked[i].classList.add('marked')
+    }
+})
